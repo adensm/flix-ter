926784a41716ac4569acd24bf8d43b118ed70975
Aden Mengistie <adensm@icloud.com>
2019-12-18 04:41:41 +0000

    Lesson Permissions

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,18 +1,38 @@
 class Instructor::SectionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def new
-    @course = Course.find(params[:course_id])
-    @section = Section.new
+    @course = Course.new
   end
 
   def create
-    @course = Course.find(params[:course_id])
-    @section = @course.sections.create(section_params)
-    redirect_to instructor_course_path(@course)
+    @course = current_user.courses.create(course_params)
+    if @course.valid?
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
   end
 
   private
 
-  def section_params
-    params.require(:section).permit(:title)
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:title, :description, :cost)
   end
 end
+
